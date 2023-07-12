075967f5ab3b41d6b330e8461b9e89f2bed3a8f9
gian <michaelgiantiqui3@gmail.com>
2023-07-12 15:09:35 +0800

    hehe

---
+++ bozo.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+#define mf int
+#define cheif main
+#define yeet return
+#define sussin if
+#define bussin for
+#define tweakin while
+#define vibin std
+#define yikes endl
+#define ongod cout
+#define fr <<
+#define deadass >>
+#define rn ;
+#define cap 0
+#define be =
+#define finna ==
+#define no !
+#define bouta ++
+#define getta cin
+#define lowdiff <
+#define joe 10
---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "bozo.cpp"
+
+mf cheif() {
+
+    mf n be cap rn
+
+    bussin (n be cap rn n lowdiff joe rn n bouta) {
+        vibin::ongod fr n rn
+    }
+
+    yeet cap rn
+}
